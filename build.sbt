0c2d0d2ca424c27f8247b086c974537244124f1b
Jakub Wojnowski <29680262+jwojnowski@users.noreply.github.com>
2021-05-20 21:35:56 +0200

    Initial commit.

---
+++ build.sbt
@@ -0,0 +1,84 @@
+name := "googlecloud4s"
+
+ThisBuild / version := "0.1.0"
+ThisBuild / isSnapshot := false
+
+idePackagePrefix := Some("me.wojnowski.googlecloud4s")
+
+val Scala212 = "2.12.12"
+val Scala213 = "2.13.5"
+
+ThisBuild / scalaVersion := Scala213
+ThisBuild / crossScalaVersions := Seq(Scala212, Scala213)
+ThisBuild / organization := "me.wojnowski"
+
+val commonSettings = Seq(
+  makePom / publishArtifact := true,
+  publishMavenStyle := true,
+  publishConfiguration := publishConfiguration.value.withOverwrite(true),
+  publishLocalConfiguration := publishLocalConfiguration.value.withOverwrite(true)
+)
+
+lazy val Versions = new {
+  val sttp = "3.2.3"
+
+  val cats = new {
+    val core = "2.1.0" // TODO these are a bit outdated
+    val effect = "2.1.2"
+  }
+
+  val log4cats = "1.0.1"
+}
+
+lazy val core = (project in file("core"))
+  .settings(
+    commonSettings ++
+      Seq(
+        name := "googlecloud4s-core",
+        libraryDependencies += "eu.timepit" %% "refined" % "0.9.21",
+        libraryDependencies += "org.typelevel" %% "cats-core" % Versions.cats.core,
+        libraryDependencies += "org.typelevel" %% "cats-effect" % Versions.cats.effect,
+        libraryDependencies += "io.chrisdavenport" %% "log4cats-core" % Versions.log4cats,
+        libraryDependencies += "io.chrisdavenport" %% "log4cats-slf4j" % Versions.log4cats,
+        libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % Versions.sttp,
+        libraryDependencies += "com.softwaremill.sttp.client3" %% "httpclient-backend-fs2" % Versions.sttp,
+        libraryDependencies += "com.softwaremill.sttp.client3" %% "circe" % Versions.sttp,
+        libraryDependencies += "io.circe" %% "circe-core" % "0.13.0",
+        libraryDependencies += "io.circe" %% "circe-generic" % "0.13.0",
+        libraryDependencies += "io.circe" %% "circe-generic-extras" % "0.13.0",
+        libraryDependencies += "io.circe" %% "circe-refined" % "0.13.0",
+        libraryDependencies += "io.circe" %% "circe-literal" % "0.13.0"
+      )
+  )
+
+lazy val auth = (project in file("auth"))
+  .dependsOn(core)
+  .settings(
+    commonSettings ++
+      Seq(
+        name := "googlecloud4s-auth",
+        libraryDependencies += "io.jsonwebtoken" % "jjwt-root" % "0.11.2",
+        libraryDependencies += "io.jsonwebtoken" % "jjwt-api" % "0.11.2",
+        libraryDependencies += "io.jsonwebtoken" % "jjwt-impl" % "0.11.2",
+        libraryDependencies += "org.scala-lang.modules" %% "scala-collection-compat" % "2.4.4"
+      )
+  )
+
+lazy val storage = (project in file("storage"))
+  .dependsOn(core, auth)
+  .settings(
+    commonSettings ++ Seq(
+      name := "googlecloud4s-storage",
+      libraryDependencies ++= List(
+        "co.fs2" %% "fs2-core",
+        "co.fs2" %% "fs2-io"
+      ).map(_ % "2.2.2")
+    )
+  )
+
+val root = project
+  .in(file("."))
+  .settings(
+    publish / skip := true
+  )
+  .aggregate(core, auth, storage)
---
+++ storage/src/main/scala/me/wojnowski/googlecloud4s/Storage.scala
@@ -0,0 +1,174 @@
+package me.wojnowski.googlecloud4s
+
+import cats.effect.Sync
+import cats.implicits._
+import fs2.Stream
+import io.chrisdavenport.log4cats.Logger
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import io.circe.Json
+import io.circe.parser.parse
+import me.wojnowski.googlecloud4s.auth.TokenProvider
+import sttp.capabilities.fs2.Fs2Streams
+import sttp.client3.SttpBackend
+import sttp.model.StatusCode
+
+import scala.util.control.NoStackTrace
+
+trait Storage[F[_]] {
+  def put(bucket: Bucket, key: Key, stream: Stream[F, Byte], maybeContentLength: Option[Long] = None): F[Unit]
+
+  def delete(bucket: Bucket, key: Key): F[Unit]
+
+  def get(bucket: Bucket, key: Key): F[Option[Stream[F, Byte]]]
+
+  def exists(bucket: Bucket, key: Key): F[Boolean]
+
+  def list(bucket: Bucket): F[List[Key]]
+}
+
+object Storage {
+
+  implicit def apply[F[_]](implicit ev: Storage[F]): Storage[F] = ev
+
+  def instance[F[_]: Sync](sttpBackend: SttpBackend[F, Fs2Streams[F]])(implicit tokenProvider: TokenProvider[F]): Storage[F] =
+    new Storage[F] {
+      import sttp.client3._
+
+      private val Scope = auth.Scope("https://www.googleapis.com/auth/devstorage.read_write")
+
+      implicit val logger: Logger[F] = Slf4jLogger.getLogger[F]
+
+      override def put(
+        bucket: Bucket,
+        key: Key,
+        stream: fs2.Stream[F, Byte],
+        maybeContentLength: Option[Long]
+      ): F[Unit] =
+        for {
+          _     <- Logger[F].debug(show"Putting [$bucket/$key]...")
+          token <- TokenProvider[F].getToken(Scope)
+          _     <- basicRequest
+                     .post(uri"https://storage.googleapis.com/upload/storage/v1/b/${bucket.value}/o?uploadType=media&name=${key.value}")
+                     .header("Authorization", s"Bearer ${token.value}")
+                     .header("Content-Type", "application/octet-stream")
+                     .streamBody(Fs2Streams[F])(stream)
+                     .send(sttpBackend)
+                     .flatMap { response =>
+                       response.body match {
+                         case Right(_)        => ().pure[F]
+                         case Left(errorBody) => UnexpectedResponseException(response, Some(errorBody)).raiseError[F, Unit]
+                       }
+                     }
+          _     <- Logger[F].info(show"Put [$bucket/$key].")
+        } yield ()
+
+      override def delete(bucket: Bucket, key: Key): F[Unit] =
+        for {
+          _     <- Logger[F].debug(show"Deleting [$bucket/$key]...")
+          token <- TokenProvider[F].getToken(Scope)
+          _     <- basicRequest
+                     .delete(uri"https://storage.googleapis.com/storage/v1/b/${bucket.value}/o/${key.value}")
+                     .header("Authorization", s"Bearer ${token.value}")
+                     .send(sttpBackend)
+                     .flatMap { response =>
+                       response.body match {
+                         case Right(_)                                        => ().pure[F]
+                         case Left(_) if response.code == StatusCode.NotFound => ().pure[F]
+                         case Left(errorBody)                                 =>
+                           UnexpectedResponseException(response, Some(errorBody)).raiseError[F, Unit]
+                       }
+                     }
+          _     <- Logger[F].debug(show"Deleted [$bucket/$key].")
+        } yield ()
+
+      override def get(bucket: Bucket, key: Key): F[Option[Stream[F, Byte]]] =
+        for {
+          _      <- Logger[F].debug(show"Getting [$bucket/$key]...")
+          token  <- TokenProvider[F].getToken(Scope)
+          result <- basicRequest
+                      .get(uri"https://storage.googleapis.com/storage/v1/b/${bucket.value}/o/${key.value}?alt=media")
+                      .header("Authorization", s"Bearer ${token.value}")
+                      .response(asStreamUnsafe(Fs2Streams[F]))
+                      .send(sttpBackend)
+                      .flatMap { response =>
+                        response.body match {
+                          case Right(stream)                                   => stream.some.pure[F]
+                          case Left(_) if response.code == StatusCode.NotFound => none[Stream[F, Byte]].pure[F]
+                          case Left(errorBody)                                 =>
+                            UnexpectedResponseException(response, Some(errorBody)).raiseError[F, Option[Stream[F, Byte]]]
+                        }
+                      }
+          _      <- Logger[F].debug(show"Got [$bucket/$key] stream")
+        } yield result
+
+      override def list(bucket: Bucket): F[List[Key]] =
+        for {
+          _      <- Logger[F].debug(show"Listing bucket [$bucket]...")
+          token  <- TokenProvider[F].getToken(Scope)
+          result <- basicRequest
+                      .get(uri"https://storage.googleapis.com/storage/v1/b/${bucket.value}/o")
+                      .header("Authorization", s"Bearer ${token.value}")
+                      .send(sttpBackend)
+                      .flatMap { response =>
+                        response.body match {
+                          case Right(data)     =>
+                            Sync[F].fromEither {
+                              parse(data).flatMap { json =>
+                                json.hcursor.downField("items").as[List[Json]].flatMap { objects =>
+                                  objects
+                                    .traverse { x =>
+                                      (x.hcursor.downField("name").as[String], x.hcursor.downField("kind").as[String]).mapN {
+                                        (name, kind) =>
+                                          if (kind === "storage#object") Some(Key(name))
+                                          else None
+                                      }
+                                    }
+                                    .map(_.collect { case Some(key) => key })
+                                }
+                              }
+                            }
+                          case Left(errorBody) =>
+                            UnexpectedResponseException(response, Some(errorBody)).raiseError[F, List[Key]]
+                        }
+                      }
+          _      <- Logger[F].debug(show"Listed bucket [$bucket].")
+        } yield result
+
+      override def exists(bucket: Bucket, key: Key): F[Boolean] =
+        for {
+          _      <- Logger[F].debug(show"Checking existence of [$bucket/$key]...")
+          token  <- TokenProvider[F].getToken(Scope)
+          result <- basicRequest
+                      .get(uri"https://storage.googleapis.com/storage/v1/b/${bucket.value}/o/${key.value}?alt=json")
+                      .header("Authorization", s"Bearer ${token.value}")
+                      .send(sttpBackend)
+                      .flatMap { response =>
+                        response.body match {
+                          case Right(_)                                        =>
+                            true.pure[F]
+                          case Left(_) if response.code == StatusCode.NotFound =>
+                            false.pure[F]
+                          case Left(errorBody)                                 =>
+                            UnexpectedResponseException(response, Some(errorBody)).raiseError[F, Boolean]
+                        }
+                      }
+        } yield result
+
+    }
+
+  case class UnexpectedResponseException(status: Int, message: String, headers: Map[String, String] = Map.empty) extends NoStackTrace {
+    override def toString: String = s"UnexpectedResponseException($status, $message, $headers)"
+  }
+
+  object UnexpectedResponseException {
+
+    def apply[T](response: sttp.client3.Response[T], message: Option[String]) =
+      new UnexpectedResponseException(
+        response.code.code,
+        message.getOrElse(response.body.toString),
+        response.headers.map(header => (header.name, header.value)).toMap
+      )
+
+  }
+
+}
